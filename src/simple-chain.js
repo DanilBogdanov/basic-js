e04e4f7522f3294c021eff5642e3166a9bf93e5d
DanilWork <bogdanovdanila1404@gmail.com>
2023-04-12 16:01:46 +0500

    feat: implement simple-chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -5,28 +5,41 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 const chainMaker = {
+  chain: [],
+
   getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.chain.length;
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  addLink(value) {
+    this.chain.push(String(value));
+    return this;
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  removeLink(position) {
+    if (Number.isInteger(position) && position -1 < this.chain.length && position -1 >= 0) {
+      this.chain.splice(position - 1, 1);
+      return this;
+    } else {
+      this.chain = [];
+      throw new Error('You can\'t remove incorrect link!');
+    }
   },
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    this.chain.reverse();
+    return this;
   },
   finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    const copyChain = [...this.chain];
+    this.chain = [];
+    return `( ${copyChain.join(' )~~( ')} )`;
   }
 };
 
 module.exports = {
   chainMaker
 };
+
+let t = chainMaker.reverseChain().reverseChain().reverseChain().addLink(NaN).reverseChain().addLink(null).addLink(1.233).addLink(true).addLink(false).removeLink(3).addLink(1.233).finishChain();
+
+let r = '+( NaN )~~( null )~~( true )~~( false )~~( 1.233 )';
+console.log(t);
+console.log(r)
